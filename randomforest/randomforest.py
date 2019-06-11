791084a9b3612ec036d63c7182bbdc7fa5a36655
Cuda-Chen <clh960524@gmail.com>
2019-06-11 19:08:17 +0800

    workable candidates

--- randomforest/randomforest.py
+++ randomforest/randomforest.py
@@ -2,6 +2,12 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
+classes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+           11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+           21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
+           31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 
+           41, 42] 
+
 lbp_train = pd.read_csv('../LBP_feature_train.csv')
 lbp_val = pd.read_csv('../LBP_feature_val.csv')
 color_som_train = pd.read_csv('../color_som_train.csv')
@@ -16,6 +22,7 @@ x_test = pd.concat([lbp_val, color_som_val], axis=1).values
 
 # convert class vectors to binary class matrices (one-hot encoding)
 # there are some missing class!
+'''
 train_label_list = train_label['class_no'].tolist()
 onehot_train = []
 for value in train_label_list:
@@ -31,9 +38,12 @@ for value in val_label_list:
     class_id[value - 1] = 1 
     onehot_val.append(class_id)
 y_test = pd.DataFrame(onehot_val)
+'''
+y_train = train_label['class_no'].values
+y_test = val_label['class_no'].values
 
 rfc = RandomForestClassifier()
 
 rfc.fit(x_train, y_train)
 
-print('The accuracy of Random Forest Classifier on testing set:', rfc.score(X_test, y_test))
+print('The accuracy of Random Forest Classifier on testing set:', rfc.score(x_test, y_test))
