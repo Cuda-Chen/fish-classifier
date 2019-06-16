95234275a7d3a760c6e2c125cfffb6ad7b79e44d
Cuda-Chen <clh960524@gmail.com>
2019-06-16 21:36:58 +0800

    add train and test script in DNN

---
+++ dnn/dnn_test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras.models import load_model
+from tensorflow.python.keras.preprocessing import image
+import sys 
+import numpy as np
+import pandas as pd
+
+# 41 + 1 classes
+cls_list = ['10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
+    '1', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29',
+    '2', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39',
+    '3', '40', '41', '42', '4', '5', '6', '7', '8', '9']
+class_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 
+    '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+    '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+    '31', '32', '33', '34', '35', '36', '37', '38', '39', '40',
+    '41', '42']
+
+lbp_test = pd.read_csv('../LBP_feature_val.csv')
+fd_test = pd.read_csv('../fd_feature_val.csv')
+color_som_test = pd.read_csv('../color_som_val.csv')
+test_label = pd.read_csv('../val_label.csv')
+
+x_test = pd.concat([lbp_test, fd_test, color_som_test], axis=1).values
+test_label_list = test_label['class_no'].tolist()
+onehot_test = []
+for value in test_label_list:
+    class_id = [0 for _ in range(len(class_list))]
+    class_id[value - 1] = 1
+    onehot_test.append(class_id)
+y_test = pd.DataFrame(onehot_test)
+
+net = load_model('my_dnn.h5')
+
+score = net.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
---
+++ dnn/dnn_train.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from __future__ import print_function
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import Adam, SGD
+
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+batch_size = 32
+num_classes = 42 # 41 + 1
+input_size = 122 # 10 + 16 * 3 + 64
+classes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+           11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+           21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+           31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+           41, 42]
+class_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 
+    '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+    '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+    '31', '32', '33', '34', '35', '36', '37', '38', '39', '40',
+    '41', '42']
+epochs = 10
+learning_rate = 1e-5
+sgb = SGD(lr=learning_rate, decay=1e-6, momentum=0.9, nesterov=True)
+
+lbp_train = pd.read_csv('../LBP_feature_train.csv')
+lbp_val = pd.read_csv('../LBP_feature_val.csv')
+fd_train = pd.read_csv('../fd_feature_train.csv')
+fd_val = pd.read_csv('../fd_feature_val.csv')
+color_som_train = pd.read_csv('../color_som_train.csv')
+color_som_val = pd.read_csv('../color_som_val.csv')
+train_label = pd.read_csv('../train_label.csv')
+val_label = pd.read_csv('../val_label.csv')
+
+# get your training and testing data here
+# and to make sure to reshape and normalize!
+x_train = pd.concat([lbp_train, fd_train, color_som_train], axis=1).values
+x_test = pd.concat([lbp_val, fd_val, color_som_val], axis=1).values
+
+# convert class vectors to binary class matrices (one-hot encoding)
+# there are some missing class!
+
+train_label_list = train_label['class_no'].tolist()
+onehot_train = []
+for value in train_label_list:
+    class_id = [0 for _ in range(len(classes))]
+    class_id[value - 1] = 1
+    onehot_train.append(class_id)
+y_train = pd.DataFrame(onehot_train)
+
+val_label_list = val_label['class_no'].tolist()
+onehot_val = []
+for value in val_label_list:
+    class_id = [0 for _ in range(len(classes))]
+    class_id[value - 1] = 1
+    onehot_val.append(class_id)
+y_test = pd.DataFrame(onehot_val)
+
+'''
+y_train = train_label.values
+y_test = val_label.values
+'''
+
+# be sure of input layer!
+model = Sequential()
+model.add(Dense(8192, activation='relu', input_shape=(input_size,)))
+model.add(Dropout(0.5))
+model.add(Dense(8192, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(2048, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+    optimizer=Adam(lr=learning_rate),
+    metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,
+    batch_size=batch_size,
+    epochs=epochs,
+    verbose=1,
+    validation_data=(x_test, y_test))
+
+print('test before save')
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+model.save('my_dnn.h5')
