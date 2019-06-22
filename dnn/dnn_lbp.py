656aafc892b52a9f8f0475cd66b3708de2564bd3
Cuda Chen <clh960524@gmail.com>
2019-06-23 03:18:56 +0800

    add training history visualization

--- dnn/dnn_lbp.py
+++ dnn/dnn_lbp.py
@@ -5,46 +5,42 @@ from __future__ import print_function
 
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, PReLU
-from keras.optimizers import Adam, SGD
+from keras.layers import Dense, Dropout
+from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
 
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
 
 batch_size = 32
-num_classes = 41 # 41
-input_size = 122 # 10 + 16 * 3 + 64
+num_classes = 41
+input_size = 10
 classes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
            11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
-           21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-           31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-           41]
-class_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 
+           21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
+           31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 
+           41] 
+class_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
     '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
     '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
     '31', '32', '33', '34', '35', '36', '37', '38', '39', '40',
     '41']
-epochs = 5000
+epochs = 500
 learning_rate = 1e-5
-sgb = SGD(lr=learning_rate, decay=1e-6, momentum=0.9, nesterov=True)
 early_stopping = EarlyStopping(monitor='val_loss', patience=100, verbose=2)
 
 lbp_train = pd.read_csv('../LBP_feature_train.csv')
 lbp_val = pd.read_csv('../LBP_feature_val.csv')
-fd_train = pd.read_csv('../fd_feature_train.csv')
-fd_val = pd.read_csv('../fd_feature_val.csv')
-color_som_train = pd.read_csv('../color_som_train.csv')
-color_som_val = pd.read_csv('../color_som_val.csv')
 train_label = pd.read_csv('../train_label.csv')
 val_label = pd.read_csv('../val_label.csv')
 
 # get your training and testing data here
 # and to make sure to reshape and normalize!
-x_train = pd.concat([lbp_train, fd_train, color_som_train], axis=1).values
-x_test = pd.concat([lbp_val, fd_val, color_som_val], axis=1).values
+#x_train = pd.concat([lbp_train, fd_train, color_som_train], axis=1).values
+#x_test = pd.concat([lbp_val, fd_val, color_som_val], axis=1).values
+x_train = lbp_train.values
+x_test = lbp_val.values
 
 # convert class vectors to binary class matrices (one-hot encoding)
 # there are some missing class!
@@ -65,11 +61,6 @@ for value in val_label_list:
     onehot_val.append(class_id)
 y_test = pd.DataFrame(onehot_val)
 
-'''
-y_train = train_label.values
-y_test = val_label.values
-'''
-
 #X_train, X_val, Y_train, Y_val = train_test_split(x_train, y_train, test_size=0.1, random_state=0)
 X_train = x_train
 X_val = x_test
@@ -78,15 +69,8 @@ Y_val = y_test
 
 # be sure of input layer!
 model = Sequential()
-model.add(Dense(8192, activation='relu', input_shape=(input_size,)))
-#model.add(PReLU())
-model.add(Dropout(0.5))
-model.add(Dense(8192, activation='relu'))
-#model.add(PReLU())
-model.add(Dropout(0.5))
-model.add(Dense(2048, activation='relu'))
-#model.add(PReLU())
-model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu', input_shape=(input_size,)))
+model.add(Dense(64, activation='relu'))
 model.add(Dense(num_classes, activation='softmax'))
 
 model.summary()
@@ -102,27 +86,7 @@ history = model.fit(X_train, Y_train,
     validation_data=(X_val, Y_val),
     callbacks=[early_stopping])
 
-# plot training & validation accuracy values
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
-
-# plot training & validation loss values
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
-
 print('test before save')
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
-print('Test accuracy:', score[1])
-
-#model.save('my_dnn.h5')
+print('Test accuracy:', score[1]) 
