cd7fcc24a98f156b4e648d21797c1c9f8bfe0b2b
Cuda-Chen <clh960524@gmail.com>
2019-06-28 18:29:42 +0800

    print predicted class that is correct

--- dnn/dnn_test.py
+++ dnn/dnn_test.py
@@ -51,6 +51,13 @@ score = net.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
+y_new = net.predict(x_test)
+np.set_printoptions(suppress=True)
+for i in range(y_new.shape[0]):
+    if ((np.argmax(y_new[i]) + 1) == test_label_nparr[i]):
+        print("Predicted=%s, predict class=%s, truth=%s, index=%s" % (y_new[i], np.argmax(y_new[i]) + 1, test_label_nparr[i], i + 1))
+
+'''
 y_pred_class = net.predict_classes(x_test)
 #print(y_pred_class.shape)
 from sklearn.metrics import confusion_matrix
@@ -61,7 +68,7 @@ cm_df = pd.DataFrame(cm, index=classList, columns=classList)
 #cm_df.index = np.arange(1, len(cm_df) + 1)
 print(cm_df)
 cm_df.to_csv('confusionMatrix.csv')
-
+'''
 '''
 pred = net.predict(x_test)[0]
 top_inds = pred.argsort()[::-1][:5]
