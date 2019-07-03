25e4a3bc0ac1eb477b9537e3449515a7a597fead
Cuda-Chen <clh960524@gmail.com>
2019-07-03 20:59:33 +0800

    ready to output experiment statistic

--- cnn/resnet50_predict.py
+++ cnn/resnet50_predict.py
@@ -4,19 +4,119 @@ from tensorflow.python.keras.preprocessing import image
 import sys
 import numpy as np
 from fishconfig import config
+import matplotlib.pyplot as plt 
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.utils.multiclass import unique_labels
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 
+# 41 classes
+classList = ['10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
+    '1', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29',
+    '2', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39',
+    '3', '40', '41', '4', '5', '6', '7', '8', '9']
+
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    #classes = classes[unique_labels(y_true, y_pred)]
+    classes = classList
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
+
+def get_metrics(y_test, y_predicted):
+    accuracy = accuracy_score(y_test, y_predicted)
+    precision = precision_score(y_test, y_predicted, average='weighted')
+    recall = recall_score(y_test, y_predicted, average='weighted')
+    f1 = f1_score(y_test, y_predicted, average='weighted')
+    return accuracy, precision, recall, f1
+
+
+DATASET_PATH = config.DATASET_PATH
 IMAGE_SIZE = config.IMAGE_SIZE
+BATCH_SIZE = config.BATCH_SIZE
 
-files = sys.argv[1:]
+#files = sys.argv[1:]
 
 net = load_model('model-resnet50-final.h5')
 
-# 41 + 1 classes
-cls_list = ['10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
-    '1', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29',
-    '2', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39',
-    '3', '40', '41', '42', '4', '5', '6', '7', '8', '9']
 
+#net = load_model('my_dnn.h5')
+
+test_datagen = ImageDataGenerator()
+test_batches = test_datagen.flow_from_directory(DATASET_PATH + '/val',
+                                                target_size=IMAGE_SIZE,
+                                                interpolation='bicubic',
+                                                class_mode='categorical',
+                                                shuffle=False)
+# output the index number of each class
+for cls, idx in test_batches.class_indices.items():
+    print('Class #{} = {}'.format(idx, cls))
+
+print('number of samples:', test_batches.samples)
+
+score = net.evaluate_generator(test_batches, steps=test_batches.samples, verbose=0)
+print('Test loss:', score[0])
+print('Test top-1 accuracy:', score[1])
+print('Test top-5 accuracy:', score[2])
+
+Y_pred = net.predict_generator(test_batches, steps=test_batches.samples)
+y_pred = np.argmax(Y_pred, axis=1)
+print('Confusion Matrix')
+print(confusion_matrix(test_batches.classes, y_pred))
+ax = plot_confusion_matrix(test_batches.classes, y_pred, classList)
+plt.show()
+plt.savefig('test.png')
+print('Classification Report')
+target_names = classList
+print(classification_report(test_batches.classes, y_pred, target_names=target_names))
+'''
 # make prediction
 for f in files:
     img = image.load_img(f, target_size=IMAGE_SIZE)
@@ -29,3 +129,4 @@ for f in files:
     print(f)
     for i in top_inds:
         print('    {:.3f}  {}'.format(pred[i], cls_list[i]))
+'''
--- cnn/resnet50_train.py
+++ cnn/resnet50_train.py
@@ -4,8 +4,20 @@ from tensorflow.python.keras.layers import Flatten, Dense, Dropout
 from tensorflow.python.keras.applications.resnet50 import ResNet50
 from tensorflow.python.keras.optimizers import Adam
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.python.keras.callbacks import EarlyStopping
+from tensorflow.python.keras.preprocessing import image
 from fishconfig import config
+from argparse import ArgumentParser
+import numpy as np
+from numpy.random import seed
+seed(1033)
+'''
+parser = ArgumentParser()
+parser.add_argument('seed', type=int, help='seed')
+args = parser.parse_args()
 
+seed(args.seed)
+'''
 DATASET_PATH = config.DATASET_PATH # dataset path
 IMAGE_SIZE = config.IMAGE_SIZE # wrap the input image size
 NUM_CLASSES = config.NUM_CLASSES
@@ -13,16 +25,19 @@ BATCH_SIZE = config.BATCH_SIZE
 FREEZE_LAYERS = config.FREEZE_LAYERS
 NUM_EPOCHS = config.NUM_EPOCHS
 WEIGHTS_FINAL = 'model-resnet50-final.h5'
+earlyStopping = EarlyStopping(monitor='val_loss', patience=100)
 
 # data agumentation
 
-train_datagen = ImageDataGenerator(rotation_range=40,
+train_datagen = ImageDataGenerator(rotation_range=90,
                                    width_shift_range=0.2,
                                    height_shift_range=0.2,
                                    shear_range=0.2,
                                    zoom_range=0.2,
                                    channel_shift_range=10,
+                                   #validation_split=0.25,
                                    horizontal_flip=True,
+                                   vertical_flip=True,
                                    fill_mode='nearest')
 '''
 train_datagen = ImageDataGenerator()
@@ -34,6 +49,7 @@ train_batches = train_datagen.flow_from_directory(DATASET_PATH + '/train',
                                                   shuffle=True,
                                                   batch_size=BATCH_SIZE)
 
+#valid_datagen = ImageDataGenerator(rescale=1./255)
 valid_datagen = ImageDataGenerator()
 valid_batches = valid_datagen.flow_from_directory(DATASET_PATH + '/val',
                                                   target_size=IMAGE_SIZE,
@@ -42,6 +58,16 @@ valid_batches = valid_datagen.flow_from_directory(DATASET_PATH + '/val',
                                                   shuffle=False,
                                                   batch_size=BATCH_SIZE)
 
+# test dataset
+'''
+test_datagen = ImageDataGenerator()
+test_batches = test_datagen.flow_from_directory(DATASET_PATH + '/val',
+                                                target_size=IMAGE_SIZE,
+                                                interpolation='bicubic',
+                                                class_mode='categorical',
+                                                shuffle=False,
+                                                batch_size=BATCH_SIZE)
+'''
 # output the index number of each class
 for cls, idx in train_batches.class_indices.items():
     print('Class #{} = {}'.format(idx, cls))
@@ -55,6 +81,10 @@ x = Flatten()(x)
 # add a dropout layer
 x = Dropout(0.5)(x)
 
+# add another Dense layer
+#x = Dense(2048, activation='relu', name='readToOutput')(x)
+#x = Dropout(0.5)(x)
+
 # add a dense layer with softmax
 output_layer = Dense(NUM_CLASSES, activation='softmax', name='softmax')(x)
 
@@ -66,18 +96,45 @@ for layer in net_final.layers[FREEZE_LAYERS:]:
     layer.trainable = True
 
 net_final.compile(optimizer=Adam(lr=1e-5),
-                  loss='categorical_crossentropy', metrics=['accuracy'])
+                  loss='categorical_crossentropy', metrics=['accuracy', 'top_k_categorical_accuracy'])
 
 print(net_final.summary())
+
+print('train samples:', train_batches.samples)
+print('validation samples:', valid_batches.samples)
+#print('test samples:', test_batches.samples)
 
 net_final.fit_generator(train_batches,
                         steps_per_epoch = train_batches.samples // BATCH_SIZE,
                         validation_data = valid_batches,
                         validation_steps = valid_batches.samples // BATCH_SIZE,
-                        epochs = NUM_EPOCHS)
+                        epochs = NUM_EPOCHS,
+                        callbacks=[earlyStopping])
 
 score = net_final.evaluate(valid_batches, steps=(valid_batches.samples / BATCH_SIZE), verbose=0)
 print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+print('Test top-1 accuracy:', score[1])
+print('Test top-5 accuracy:', score[2])
+
+# evaluate!
+# 41 classes
+classList = ['10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
+    '1', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29',
+    '2', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39',
+    '3', '40', '41', '4', '5', '6', '7', '8', '9']
+f = '七星斑.jpg'
+img = image.load_img(f, target_size=IMAGE_SIZE)
+if img is None:
+    print('input image not found')
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+pred = net_final.predict(x)[0]
+top_inds = pred.argsort()[::-1][:5]
+print(f)
+print(pred.shape)
+print(top_inds.shape)
+for i in top_inds:
+    print('    {:.3f}  {}'.format(pred[i], classList[i]))
 
 net_final.save(WEIGHTS_FINAL)
+#np.savetxt(str(args.seed), score, fmt='%1.4f')
